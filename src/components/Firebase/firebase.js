098a1f3e8cee1b3f85ad6a2c0d94c7a90e364bce
Cory Milsap <crmilsap@crimson.ua.edu>
2020-12-11 20:20:11 -0600

    Init Firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,35 @@
+import app from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+// import 'firebase/functions';
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+  apiKey: "AIzaSyCPc8HYjEVQlp_xTgY-Nx2LBA_F5K52zDE",
+  authDomain: "gs-hackathon.firebaseapp.com",
+  projectId: "gs-hackathon",
+  storageBucket: "gs-hackathon.appspot.com",
+  messagingSenderId: "505118643224",
+  appId: "1:505118643224:web:4b088d81cd6b9c72e35f6d",
+  measurementId: "G-X3VLM33XE4"
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.app = app;
+    // this.auth = app.auth();
+    // this.db = app.firestore();
+    // this.functions = app.functions();
+
+    // this.perf = app.performance();
+    // this.analytics = app.analytics();
+
+    // new app.auth.GoogleAuthProvider();
+    // new app.auth.EmailAuthProvider();
+  }
+}
+
+export default Firebase
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Firebase
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 ReactDOM.render(
-  <React.StrictMode>
+      <FirebaseContext.Provider value = {new Firebase()}>
     <App />
-  </React.StrictMode>,
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
