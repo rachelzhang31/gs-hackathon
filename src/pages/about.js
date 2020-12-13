d02849716f7513d6060c80cd0c1cb40e380bc068
Rachel Zhang <34756692+rachelzhang31@users.noreply.github.com>
2020-12-12 20:39:25 -0500

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ class About extends Component {
                 <p style={{display:'inline-block'}}>Cindy Ding, Charles Kraemer, Cory Milsap, and Rachel Zhang</p>
                 <br></br>
                 <br></br>
-                <p>We created this website to visualize the impact of COVID-19 on the U.S. economy. Our goal was to make it digestible for people of all ages and technical backgrounds.</p>
+                <p>We created this website to visualize the impact of COVID-19 on different sections of the US population with a special focus on unemployed and incarcerated individuals, who have been affected disproportionately by the pandemic. We also explored mobility trends over time. Our goal was to present this information in a digestible format for people of all ages and technical backgrounds.</p>
             </div>
         );
     }
